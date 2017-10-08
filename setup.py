20cb4c725ff10c2150566fb2b49826f70e1150b6
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2017-10-08 20:08:11 +0300

    Try to build on windows

--- setup.py
+++ setup.py
@@ -2,6 +2,14 @@ from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 import sys
 import setuptools
+import os
+import subprocess
+
+try:
+	from urllib.request import urlretrieve
+except ImportError:
+	from urllib import urlretrieve
+import zipfile
 
 __version__ = '0.0.1'
 
@@ -19,14 +27,42 @@ class get_pybind_include(object):
         import pybind11
         return pybind11.get_include(self.user)
 
+def download_eigen():
+	zippath = os.path.join('deps', 'eigen.zip')
+	if not os.path.exists('deps'): os.mkdir('deps')
+	if not os.path.exists(zippath):
+		urlretrieve("http://bitbucket.org/eigen/eigen/get/3.3.4.zip", zippath)
+	
+	f = zipfile.ZipFile(zippath)
+	f.extractall('deps')
+	return os.path.join('deps', "eigen-eigen-5a0156e40feb")
+
+class DownloadEigen(setuptools.Command):
+	"""Download eigen headers"""
+	
+	# OMG how setuptools suck. This is
+	# Java-level horror
+	user_options = []
+	def initialize_options(self): pass
+	def finalize_options(self): pass
+	
+	def run(self):
+		return download_eigen()
+
+def get_eigen():
+	if 'EIGEN3_INCLUDE_DIR' in os.environ:
+		return os.environ['EIGEN3_INCUDE_DIR']
+	return download_eigen()
 
 ext_modules = [
     Extension(
         'nslr',
         ['python/nslr.cpp'],
         include_dirs=[
-            # Change this according to your Eigen installation
-            '/usr/include/eigen3',
+            # Try to get eigen include directory. Set
+	    # EIGEN3_INCLUDE_DIR environment variable to
+	    # set manually.
+	    get_eigen(),
             # These come from the pybind11 python package
             # (eg. pip install pybind11)
             get_pybind_include(),
@@ -67,9 +103,10 @@ def cpp_flag(compiler):
 
 class BuildExt(build_ext):
     """A custom build extension for adding compiler-specific options."""
+    defopts = []
     c_opts = {
-        'msvc': ['/EHsc'],
-        'unix': [],
+        'msvc': ['/EHsc'] + defopts,
+        'unix': defopts[:],
     }
 
     if sys.platform == 'darwin':
@@ -99,6 +136,6 @@ setup(
     long_description='',
     ext_modules=ext_modules,
     install_requires=[],
-    cmdclass={'build_ext': BuildExt},
+    cmdclass={'build_ext': BuildExt, 'download_eigen': DownloadEigen},
     zip_safe=False,
 )
